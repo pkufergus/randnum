08f41c1681195be2b8c911b02df8214b021909ae
Cao Song <caosong@baidu.com>
2017-12-14 00:49:14 +0800

    get 7600
    Signed-off-by: Cao Song <caosong@baidu.com>

--- rand.sh
+++ rand.sh
@@ -1,6 +1,9 @@
 
 file1="shiyan.txt"
 file2="duizhao.txt"
+file3="backup/duizhao.txt"
+file4="backup/shiyan.txt"
+file5="backup/zhaozhuang.sid"
 
 i=0
 out1="out1"
@@ -8,6 +11,23 @@ out2="out2"
 out3="out3"
 rm -f $out1
 c=0
+echo "c=$c"
+for i in `seq 1 5000`
+do
+    if [ "$i" == "2011" -o "$i" == "1753" -o "$i" == "2489" -o "$i" == "936" ]; then
+        continue
+    fi
+    grep ",$i," backup/zhaozhuang.sid  >/dev/null 2>&1
+    ret1=$?
+    if [ "$ret1" != "0" ]; then
+        printf "$i," >> $out1
+        c=$(($c+1))
+        if [ "$c" -ge 8000 ]; then
+            break
+        fi
+    fi
+done
+echo "c=$c"
 for i in `seq 5001 6000`
 do
     grep $i $file1 >/dev/null 2>&1
@@ -17,25 +37,27 @@ do
     if [ "$ret1" != "0" -a "$ret2" != "0" ]; then
         printf "$i," >> $out1
         c=$(($c+1))
-        if [ "$c" -ge 6000 ]; then
+        if [ "$c" -ge 8000 ]; then
             break
         fi
     fi
 done
 
-for i in `seq 6001 10000`
+echo "c=$c"
+for i in `seq 6001 9999`
 do
     grep $i $file1 >/dev/null 2>&1
     ret1=$?
     grep $i $file2 >/dev/null 2>&1
     ret2=$?
-    grep -r $i $file2 >/dev/null 2>&1
+    grep -r $i backup/  >/dev/null 2>&1
     ret3=$?
     if [ "$ret1" != "0" -a "$ret2" != "0" -a "$ret3" != "0" ]; then
         printf "$i," >> $out1
         c=$(($c+1))
-        if [ "$c" -ge 6000 ]; then
+        if [ "$c" -ge 8000 ]; then
             break
         fi
     fi
 done
+echo "c=$c"
