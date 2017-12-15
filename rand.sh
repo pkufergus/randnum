e81bb2a38435dde4b8f3e38a949a126b88f1acab
Cao Song <caosong@baidu.com>
2017-12-15 14:50:56 +0800

    get 4000
    Signed-off-by: Cao Song <caosong@baidu.com>

--- rand.sh
+++ rand.sh
@@ -12,22 +12,7 @@ out3="out3"
 rm -f $out1
 c=0
 echo "c=$c"
-for i in `seq 1 5000`
-do
-    if [ "$i" == "2011" -o "$i" == "1753" -o "$i" == "2489" -o "$i" == "936" ]; then
-        continue
-    fi
-    grep ",$i," backup/zhaozhuang.sid  >/dev/null 2>&1
-    ret1=$?
-    if [ "$ret1" != "0" ]; then
-        printf "$i," >> $out1
-        c=$(($c+1))
-        if [ "$c" -ge 8000 ]; then
-            break
-        fi
-    fi
-done
-echo "c=$c"
+total_num=4000
 for i in `seq 5001 6000`
 do
     grep $i $file1 >/dev/null 2>&1
@@ -37,7 +22,7 @@ do
     if [ "$ret1" != "0" -a "$ret2" != "0" ]; then
         printf "$i," >> $out1
         c=$(($c+1))
-        if [ "$c" -ge 8000 ]; then
+        if [ "$c" -ge $total_num ]; then
             break
         fi
     fi
@@ -53,11 +38,27 @@ do
     grep -r $i backup/  >/dev/null 2>&1
     ret3=$?
     if [ "$ret1" != "0" -a "$ret2" != "0" -a "$ret3" != "0" ]; then
-        printf "$i," >> $out1
-        c=$(($c+1))
-        if [ "$c" -ge 8000 ]; then
+        if [ "$c" -ge $total_num ]; then
             break
         fi
+        printf "$i," >> $out1
+        c=$(($c+1))
+    fi
+done
+echo "c=$c"
+for i in `seq 1 5000`
+do
+    if [ "$i" == "2011" -o "$i" == "1753" -o "$i" == "2489" -o "$i" == "936" ]; then
+        continue
+    fi
+    grep ",$i," backup/zhaozhuang.sid  >/dev/null 2>&1
+    ret1=$?
+    if [ "$ret1" != "0" ]; then
+        if [ "$c" -ge $total_num ]; then
+            break
+        fi
+        printf "$i," >> $out1
+        c=$(($c+1))
     fi
 done
 echo "c=$c"
